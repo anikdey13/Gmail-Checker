50d1793afc15fdfb425db3aefc6112c21aca9736
Anik <deyanik659@gmail.com>
2024-04-07 12:17:08 +0600

    home page design update

--- home.php
+++ home.php
@@ -28,6 +28,98 @@
                     </div>
                 </div>
             </div>
+            <div class="col-lg-6">
+                <div class="output__section">
+                    <div class="input__section__account-info mb-3">
+                        <p>Admin</p>
+                        <span>Server 1: gmailchecker.drboxstore.com - </span>
+                        <span>Server 2: gmailscan.com - Telegram</span>
+                    </div>
+                    <div class="input__section__text-area">
+                        <textarea name="" id="" cols="30" rows="10" placeholder="Output mail here"></textarea>
+                    </div>
+                    <div class="output__section-list">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Good</span>
+                                    <span></span>
+                                    <span class="badge text-bg-primary">0</span>
+                                </div>
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Disable</span>
+                                    <span></span>
+                                    <span class="badge text-bg-secondary">0</span>
+                                </div>
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Unknown</span>
+                                    <span></span>
+                                    <span class="badge text-bg-success">0</span>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Verify</span>
+                                    <span></span>
+                                    <span class="badge text-bg-info">0</span>
+                                </div>
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Not exists</span>
+                                    <span></span>
+                                    <span class="badge text-bg-warning">0</span>
+                                </div>
+                                <div class="item-wrapper d-flex border ">
+                                    <span><i class="fa-solid fa-circle"></i>Wrong format</span>
+                                    <span></span>
+                                    <span class="badge text-bg-danger">0</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input__section__progress-bar">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="home-page-cards py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="card-wrapper">
+                    <span><i class="fa-solid fa-circle-dollar-to-slot"></i></span>
+                    <p>hotmail checker, outlook checker</p>
+                    <h4>35$/month</h4>
+                    <a href="#">Buy now</a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card-wrapper">
+                    <span><i class="fa-solid fa-circle-dollar-to-slot"></i></span>
+                    <p>24h gmail ~ 7.9$/100</p>
+                    <h4>New Gmail ~ 18$/100</h4>
+                    <a href="#">Buy now</a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card-wrapper">
+                    <span><i class="fa-solid fa-circle-dollar-to-slot"></i></span>
+                    <p>Gmail 2015 - 2018</p>
+                    <h4>Gmail 2019 - 2022</h4>
+                    <a href="#">Buy now</a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card-wrapper">
+                    <span><i class="fa-solid fa-circle-dollar-to-slot"></i></span>
+                    <p>USA Gmail 1 month</p>
+                    <h5>Gmail USA 2014 for GV 100%</h5>
+                    <a href="#">Buy now</a>
+                </div>
+            </div>
         </div>
     </div>
 </section>
